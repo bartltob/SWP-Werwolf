bc2097e976bdac3b6c25c82c0b2930152a71f031
Luca Hagspiel <luca.hagspiel@gmail.com>
2026-02-18 11:53:12 +0100

    remove Player onDisconnect added

--- Werwolf/src/Pages/GamePage.tsx
+++ Werwolf/src/Pages/GamePage.tsx
@@ -1,19 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import WaitingRoom from "../Components/WaitingRoom";
+import { ref, onDisconnect } from "firebase/database";
+import { db } from "../firebase-config";
 
-type Props = {};
-
-export default function GamePage({}: Props) {
-
+export default function GamePage() {
     const [isWaiting] = useState(true);
+
+    useEffect(() => {
+        const roomKey = localStorage.getItem("roomKey");
+        const playerId = localStorage.getItem("playerId");
+
+        if (!roomKey || !playerId) return;
+
+        const playerRef = ref(db, `rooms/${roomKey}/players/${playerId}`);
+
+        onDisconnect(playerRef).remove().catch((err) => {
+            console.error("Failed to setup onDisconnect:", err);
+        });
+
+    }, []);
 
     if (isWaiting) return <WaitingRoom />;
 
-    return (
-        <div>
-
-
-
-        </div>
-    );
+    return <div></div>;
 }
