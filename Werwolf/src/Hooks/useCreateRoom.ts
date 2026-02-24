3d967c43a021d25b8f8b131e4536cb5d5924e659
Luca Hagspiel <luca.hagspiel@gmail.com>
2026-02-24 12:16:42 +0100

    create room fix

--- Werwolf/src/Hooks/useCreateRoom.ts
+++ Werwolf/src/Hooks/useCreateRoom.ts
@@ -1,9 +1,19 @@
-import { createRoomInDb } from '../Services/roomService';
+import { ref, set } from 'firebase/database';
+import { db } from '../firebase-config';
 
 export const useCreateRoom = () => {
 
   const createRoom = async () => {
-    return await createRoomInDb();
+      const roomKey = Math.floor(Math.random() * 900000) + 100000;
+      const roomRef = ref(db, `rooms/${roomKey}`);
+
+      await set(roomRef, {
+          created_at: new Date().toISOString(),
+          players: {},
+      });
+
+      sessionStorage.setItem('roomKey', String(roomKey));
+      return String(roomKey);
   };
 
   return { createRoom };
